465df636f8d79a0c0d65b07eb9214f52a28397d8
Baxtiyorjon-Dev <baxtiyorjon.dev@gmail.com>
2024-02-09 20:14:06 +0500

    third commit

--- src/main/java/com/dataspin/oboiservice/OboiserviceApplication.java
+++ src/main/java/com/dataspin/oboiservice/OboiserviceApplication.java
@@ -2,12 +2,26 @@ package com.dataspin.oboiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OboiserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OboiserviceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer configureCORS() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+            }
+        };
     }
 
 }
